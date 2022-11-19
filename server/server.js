abf9378ede81382f82fc6de7f8e6adede8fa5411
BryanBH <bryanbenjumea@gmail.com>
2022-11-19 18:10:13 -0500

    heroku errors fixed

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 4000;
