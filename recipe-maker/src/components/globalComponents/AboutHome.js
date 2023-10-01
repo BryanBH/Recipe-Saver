5b253b4fc940bc7983b7c21507400495b34bae5e
BryanBH <bryanbenjumea@gmail.com>
2023-09-30 21:08:25 -0400

    updated about card info

--- recipe-maker/src/components/globalComponents/AboutHome.js
+++ recipe-maker/src/components/globalComponents/AboutHome.js
@@ -6,8 +6,8 @@ const AboutHome = () => {
     <Container className="text-center text-light bg-darkAccent rounded-5 shadow-sm" style={{maxWidth:"700px"}}>
       <Row>
         <Col>
-          <p className="p-3 mt-3">
-           The idea of this application is to help those who want to try new recipes let it be for yourself, for friends & family, or to challenge your cooking skills! Sign up and save your favorite recipes for future cooking/baking 😀
+          <p className="p-3 mt-3 fs-5">
+           Ever wanted to try new recipes? let it be for yourself, for friends & family, or to challenge your cooking skills! Sign up and save your favorite recipes for future cooking/baking.
           </p>
         </Col>
       </Row>
--- recipe-maker/src/components/globalComponents/carousel/CarouselCard.js
+++ recipe-maker/src/components/globalComponents/carousel/CarouselCard.js
@@ -1,33 +1,36 @@
-import React from "react";
-import { Card } from "react-bootstrap";
-import "./carouselCard.css";
-import { Link } from "react-router-dom";
-import imagePlaceholder from "../../../images/No-Image-Placeholder.svg.png";
-import DeleteRecipeButton from "../deleteRecipeButton/DeleteRecipeButton";
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import './carouselCard.css';
+import { Link } from 'react-router-dom';
+import imagePlaceholder from '../../../images/No-Image-Placeholder.svg.png';
+import DeleteRecipeButton from '../deleteRecipeButton/DeleteRecipeButton';
 const CarouselCard = ({ recipe, index }) => {
   return (
     <>
-      <Card className="carousel-card d-flex justify-content-center align-items-center mx-3 shadow-sm">
+      <Card className='carousel-card d-flex justify-content-center align-items-center mx-3 shadow-sm'>
         <Link to={`/recipe/${recipe.id}`}>
           <Card.Img
-            variant="top"
-            className="mt-2 px-2 rounded-3"
+            variant='top'
+            className='mt-2 px-2 rounded-3'
             src={recipe.image ? recipe.image : imagePlaceholder}
-            style={{ height: "250px", width: "100%" }}
+            style={{ height: '250px', width: '100%' }}
           />
           <Card.Body
-            className=""
-            style={{ height: "200px", overFlowY: "auto" }}>
-            <Card.Title className="text-center">
+            className=''
+            style={{ height: '200px', overFlowY: 'auto' }}>
+            <Card.Title className='text-center'>
               <strong>{recipe.title}</strong>
             </Card.Title>
             <Card.Text>
               {recipe.readyInMinutes ? (
-                <>
-                  Ready in: {recipe.readyInMinutes} minutes <br></br>
-                </>
+                <p className='mb-1 fs-5'>
+                  {' '}
+                  Ready in: {recipe.readyInMinutes} minutes{' '}
+                </p>
               ) : null}
-              {recipe.servings ? <>Servings: {recipe.servings}</> : null}
+              {recipe.servings ? (
+                <p className='fs-5'>Servings: {recipe.servings}</p>
+              ) : null}
             </Card.Text>
           </Card.Body>
         </Link>
